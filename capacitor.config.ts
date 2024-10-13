b28b7155d2a1a1718e3bb7dc41b2c9a50c52b197
Damaceno25 <damaceno2590@gmail.com>
2024-10-13 09:43:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ZarApp-01',
+  webDir: 'www'
+};
+
+export default config;
